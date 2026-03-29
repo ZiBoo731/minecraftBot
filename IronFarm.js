3d9f9ce06d8df4d1f62b5bcc0dbcd08f0cbb110c
ZiBoo731 <piotrsprysak7283@gmail.com>
2026-03-29 13:17:50 +0100

    Refactor IronFarm.js server and message handling

--- IronFarm.js
+++ IronFarm.js
@@ -1,13 +1,12 @@
 const http = require('http')
 const mineflayer = require('mineflayer')
 
-const port = process.env.PORT || 10000
+
 http.createServer((req, res) => {
-  res.write("Bot IronFarm is alive!")
-  res.end()
-}).listen(port, () => {
-  console.log(`Serwer HTTP nasluchuje na porcie ${port}`)
-})
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Bot is running!')
+}).listen(process.env.PORT || 3000)
+
 
 const options = {
   host: 'klakier-rudy-kot.csrv.gg',
@@ -21,34 +20,31 @@ const bot = mineflayer.createBot(options)
 
 
 bot.on('spawn', () => {
-    bot.chat('hi!')
-bot.setQuickBarSlot(8)
+  bot.chat('hi!')
+  bot.setQuickBarSlot(8)
+  console.log('Bot is runing')
 })
 
 
 bot.on('messagestr', (message) => {
-    if(message.includes('eat')){
-        const hunger = bot.food
-        if(hunger<20){
-            bot.chat("I'm hungry (level: "+hunger+"), I'm eating")
-            bot.consume()
-        }else(
-            bot.chat("I'm not hungry(level: "+hunger+")")
-        )
-    }
-})
 
-bot.on('messagestr', (message) => {
-    if(message.includes('leave')){
+  if(message.includes('leave')){
         bot.quit()
         process.exit();
-    }
-})
+  }
 
-bot.on('messagestr', (message) => {
-    if(message.includes('slot')){
-        bot.chat(`hand is at : ${bot.quickBarSlot} place`)
-    }
+  else if(message.includes('slot')){
+    bot.chat(`hand is at : ${bot.quickBarSlot} place`)
+  }
+
+  else if(message.includes('eat')){
+    if(hunger<20){
+      bot.chat("I'm hungry (level: "+hunger+"), I'm eating")
+      bot.consume()
+    }else{
+      bot.chat("I'm not hungry(level: "+hunger+")")
+    } 
+  }
 })
 
 
